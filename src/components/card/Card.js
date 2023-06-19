8c830ac4cfc348b6d16ac9b52d04576a6ee59a70
Yusuf Gözükara <101390306+yusufgozukara@users.noreply.github.com>
2023-06-19 11:35:23 +0300

    step3

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./Card.css";
+import { languages } from "../../helpers/data";
+import Item from "../item/Item";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="cards-area-container">
+      <div className="bars"></div>
+      <h1 className="language-title">Languages</h1>
+      <div className="cards-container">
+        {languages.map((item, index) => {
+          return <Item card={item} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,29 +1,12 @@
-import { useState } from 'react';
-import './Item.css';
+import React from "react";
+import "./Item.css";
 
 const Item = ({ card }) => {
-  const [showLogo, setShowLogo] = useState(true);
-
   const { name, img, options } = card;
-
-  const handleClick = () => {
-    setShowLogo(!showLogo);
-  };
-
   return (
-    <div className="card" onClick={handleClick}>
-      {showLogo ? (
-        <div>
-          <img className="card-logo" src={img} alt="" />
-          <h3 className="card-title">{name}</h3>
-        </div>
-      ) : (
-        <ul className="list">
-          {options.map((element, index) => {
-            return <li key={index}>{element}</li>;
-          })}
-        </ul>
-      )}
+    <div className="card">
+      <img className="card-logo" src={img} alt="lang-logo" />
+      <h3 className="card-title">{name}</h3>
     </div>
   );
 };
