07af34e307743a19f126f37b8e0a13cfcb7fd46e
Yusuf Gözükara <101390306+yusufgozukara@users.noreply.github.com>
2023-06-19 10:05:27 +0300

    header fin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
-import Card from './components/card/Card';
-import Header from './components/header/Header';
+import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div className="main-container">
+    <div>
       <Header />
-      <Card />
     </div>
   );
 }
